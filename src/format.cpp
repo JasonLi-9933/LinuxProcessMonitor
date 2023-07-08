776c541e0b82a150cef0a17f112958d677559299
JasonLi9933 <haotian-li@outlook.com>
2023-07-07 23:06:34 -0700

    modify the time format method to use chrono API

--- src/format.cpp
+++ src/format.cpp
@@ -1,18 +1,29 @@
-#include <string>
-
 #include "format.h"
 
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std::chrono;
 using std::string;
 using std::to_string;
 
-// TODO: Complete this helper function
+#define CHRONO_DURACTION_CAST(type) \
+  std::chrono::duration_cast<std::chrono::type>
+// Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds) { 
-    long SS = seconds % SECS_IN_MINUTE;
-    seconds -= SS;
-    long MM = (seconds % SECS_IN_HOUR) / SECS_IN_MINUTE;
-    long HH = seconds / SECS_IN_HOUR;
-    return to_string(HH) + ":" + to_string(MM) + ":" + to_string(SS); 
+string Format::ElapsedTime(long seconds) {
+  std::chrono::seconds secs{seconds};
+  std::chrono::hours HH = CHRONO_DURACTION_CAST(hours)(secs);
+  secs -= CHRONO_DURACTION_CAST(seconds)(HH);
+  std::chrono::minutes MM = CHRONO_DURACTION_CAST(minutes)(secs);
+  secs -= CHRONO_DURACTION_CAST(seconds)(MM);
+  std::stringstream ss{};
+  ss << std::setw(2) << std::setfill('0') << HH.count() << std::setw(1) << ":"
+     << std::setw(2) << std::setfill('0') << MM.count() << std::setw(1) << ":"
+     << std::setw(2) << std::setfill('0') << secs.count();
+  return ss.str()
 }
